c56213bd3d6c52e52c11361e7e843136a404be3c
enesboz-9 <154803521+enesboz-9@users.noreply.github.com>
2026-03-31 17:16:28 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.set_page_config(
 # Load custom CSS
 with open("style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
-
+from data.oxford import OXFORD_3000
 from data.vocabulary import VOCABULARY
 from data.sentences import SENTENCE_PATTERNS
 from data.adjectives import ADJECTIVES, VERBS, ADVERBS
